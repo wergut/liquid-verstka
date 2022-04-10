564f4050629d5ecce5619d6f528cf724d6eb6ccd
wergut <web-style.programming@mail.ru>
2022-04-10 16:05:29 +0500

    10.04.2022

--- src/js/index.js
+++ src/js/index.js
@@ -44,6 +44,10 @@ $(document).ready(function() {
         thumbs: {
           swiper: swiper,
         },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+        },
     });
 });
 
@@ -81,3 +85,28 @@ $(document).ready(function() {
         mobileFilter[0].classList.toggle("show");
     }
     
+
+
+    function handleFileSelect(evt) {
+    var file = evt.target.files; // FileList object
+    // Loop through the FileList and render image files as thumbnails.
+    for (var i = 0, f; f = files[i]; i++) {
+        // Only process image files.
+        if (!f.type.match('image.*')) {
+            alert("Image only please....");
+        }
+        var reader = new FileReader();
+        // Closure to capture the file information.
+        reader.onload = (function (theFile) {
+            return function (e) {
+                // Render thumbnail.
+                var span = document.createElement('span');
+                span.innerHTML = ['<img class="thumb" title="', escape(theFile.name), '" src="', e.target.result, '" />'].join('');
+                document.getElementById('output').insertBefore(span, null);
+            };
+        })(f);
+        // Read in the image file as a data URL.
+        reader.readAsDataURL(f);
+    }
+}
+document.getElementById('file').addEventListener('change', handleFileSelect, false);
